19a9e1bb95faf09bd503b77d50e39c431dbb6c7a
Anhar Miah <anharm1ah@protonmail.com>
2026-07-06 18:24:20 +0100

    revert BASE_URL

--- sample-web/src/config.js
+++ sample-web/src/config.js
@@ -1,5 +1,5 @@
 export default {
-    // BASE_URL: 'https://neo.wotfood.co.uk',
-    BASE_URL: 'http://localhost:8080',
+    BASE_URL: 'https://neo.wotfood.co.uk',
+    // BASE_URL: 'http://localhost:8080',
     COMPANY_ID: 10
 }
